392b47853d100a7081ba65653ec711d129d81b3a
ddwooten <danieldavidwooten@gmail.com>
2020-06-28 14:14:36 -0700

    Trying a slash again

--- assets/js/submit_to_ec2.js
+++ assets/js/submit_to_ec2.js
@@ -5,7 +5,7 @@ function submit_to_ec2() {
 
 	const httpObj = new XMLHttpRequest();
 
-	url = "https://ec2-52-86-185-169.compute-1.amazonaws.com:5000/chatbot";
+	url = "https://ec2-52-86-185-169.compute-1.amazonaws.com:5000/chatbot/";
 
 	url_test = "https://ipinfo.io/json";
 
