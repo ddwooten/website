dd8a85ec11fa928043d38b2cf002b7865fa2f55d
ddwooten <danieldavidwooten@gmail.com>
2020-06-30 11:02:50 -0700

    typo

--- assets/js/submit_to_ec2.js
+++ assets/js/submit_to_ec2.js
@@ -11,7 +11,7 @@ function submit_to_ec2() {
 
 	httpObj.setRequestHeader('Content-type', 'application/json')
 
-	httpObj.send(JSON.strinify({passValue:'penguin'}));
+	httpObj.send(JSON.stringify({passValue:'penguin'}));
 
 	httpObj.onreadystatechange = function(){
 
